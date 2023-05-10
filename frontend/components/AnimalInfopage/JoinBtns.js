b822513b8d7cf19a1d80a99e0187991a06f54c2f
Shannon1923 <86880073+Shannon1923@users.noreply.github.com>
2023-05-10 16:40:03 +0800

    animalInfoPage v1.1
    整理架構與排版，更新README

---
+++ frontend/components/AnimalInfopage/JoinBtns.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { brownTheme } from "@/styles/jss/components/AnimalInfoPage/InfoPageStyle";
+import { ThemeProvider } from '@mui/material/styles';
+import { Typography, Link, Button } from '@mui/material';
+
+export default function JoinBtns({firstAdoptionStatus}) {
+  return (
+    <div>
+        {firstAdoptionStatus !== "審核中" && (
+          <>
+          {/* 加入認養計畫按鈕 */}
+          {firstAdoptionStatus === "認養中" ? (
+            <ThemeProvider theme={brownTheme}>
+              <Button href="/ExtendAdopt" variant="contained" color="primary" sx={{ mt:2 }}>
+                延長認養計畫
+              </Button>
+            </ThemeProvider>
+          ) : (
+            <ThemeProvider theme={brownTheme}>
+              <Button href="/JoinAdopt" variant="contained" color="primary" sx={{ mt:2 }}>
+                加入認養計畫
+              </Button>
+            </ThemeProvider>
+          )}
+          <Typography variant="body2" component="p" sx={{ mt:1.5 }}>
+            <Link href="/faq">了解認養流程</Link>
+          </Typography>
+          </>
+        )}
+    </div>
+  )
+}
