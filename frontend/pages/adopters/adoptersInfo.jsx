37f5de7b3382863e175e1be5e271b47f3d872a03
angel870326 <angel870326@gmail.com>
2023-05-11 01:07:53 +0800

    add adopt info blank components

--- frontend/pages/adopters/adoptersInfo.jsx
+++ frontend/pages/adopters/adoptersInfo.jsx
@@ -1,10 +1,16 @@
+
+import React from "react"
 import Layout from '@/components/Layout'
+import { title, content } from "@/styles/jss/components/AnimalListpage/generalStyle";
+import AdopterInfo from '@/components/AdopterInfoPage/AdopterInfo'
+import AdopterAnimalInfo from '@/components/AdopterInfoPage/AdopterAnimalInfo'
 
 
 export default function AdoptersInfoPage() {
 
   return (
     <Layout>
+      <h1 style={title}>認養人資訊</h1>
       add adopters info page here.
     </Layout>
 
