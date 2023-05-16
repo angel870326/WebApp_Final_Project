0216fddbdfbc104b554c5ed8257dbb0b113dbc69
angel870326 <angel870326@gmail.com>
2023-05-16 17:01:16 +0800

    Add Editting Account Info Page

--- frontend/pages/account/edit.jsx
+++ frontend/pages/account/edit.jsx
@@ -1,16 +1,13 @@
 import React from "react"
 import Layout from '@/components/Layout'
-import { Grid } from "@mui/material";
 import { title } from "@/styles/jss/animal-cloud-adoption.js";
-import AccountInfo from '@/components/AccountPage/AccountInfo'
+import AccountInfoEdit from '@/components/AccountPage/AccountInfoEdit'
 
 export default function AccountPage() {
-
   return (
     <Layout>
-      <h1 style={title}>編輯個人資料</h1>
-      暫時放著還沒改
-      <AccountInfo />
+      <h1 style={title}>我的頁面</h1>
+      <AccountInfoEdit />
     </Layout>
   )
 }
